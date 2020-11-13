9f6a08da4e8df4eaa80db61a78b937a53dc1ef12
Hugo Ferrer <hugo-ferrer@hotmail.fr>
2020-11-13 14:11:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from microbit import *
 from ssd1306 import initialize, clear_oled
 from ssd1306_text import add_text
 
-initialize()
-clear_oled()
-add_text(0, 0, "Salut !!!")
+initialize(pinReset=pin0)
+
+while True :
+    clear_oled()
+    add_text(0, 0, "Temp : "+str(temperature())+" C")
+    add_text(0, 1, "Lum : "+str(int((display.read_light_level()/255)*100))+" %")
+    sleep(20000)
