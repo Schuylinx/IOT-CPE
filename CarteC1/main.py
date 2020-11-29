a942c64ce9db4a708224da0845fc2869afbe53e7
Hugo Ferrer <hugo-ferrer@hotmail.fr>
2020-11-29 15:51:00 +0100

    fewer lines of code to save memory

--- CarteC1/main.py
+++ CarteC1/main.py
@@ -26,54 +26,29 @@ def displayToScreen(order): # order is a string
         add_text(0, 1, "Temp : "+str(temperature())+" C  ")
 
 def getJSONToSend():
-    json = "{"
-    json += "\"source\":\""+CARD_ID+"\","
-    json += "\"destination\":\""+CARD_DEST+"\","
-    json += "\"password\":\""+NWK_IDFR+"\","
-    json += "\"data\":["
-    json += str(temperature())+","
-    json += str(int((display.read_light_level()/255)*100))
-    json += "]"
-    json += "}"
+    json = "{\"source\":\""+CARD_ID+"\",\"destination\":\""+CARD_DEST+"\",\"password\":\""+NWK_IDFR+"\",\"data\":["+str(temperature())+","
+    json += str(int((display.read_light_level()/255)*100))+"]}"
     return json
 
 def parseData(json):
-    tmp = json[1:len(json)-1]
-    tmp = tmp.replace("\"", "")
-    tmp = tmp.split(",")
-    temperature = tmp[3].split(":")[1].replace("[", "")
-    light = tmp[4].replace("]", "")
-    data = [temperature, light]
+    tmp = json[1:len(json)-1].replace("\"", "").split(",") 
+    data = [tmp[3].split(":")[1].replace("[", ""), tmp[4].replace("]", "")] # [temperature, light]
     return data
 
 def parsePassword(json):
-    tmp = json[1:len(json)-1]
-    tmp = tmp.replace("\"", "")
-    tmp = tmp.split(",")
-    password = tmp[2].split(":")[1]
+    password = json[1:len(json)-1].replace("\"", "").split(",")[2].split(":")[1]
     return password
-    
+
 def checkPassword(password):
     return password == NWK_IDFR
 
 while True:
     incomingJSON = radio.receive()
     if incomingJSON != None:
-        parsePassword(incomingJSON)
+        #incomingJSON = decrypt(incomingJSON, 1)
         if checkPassword(parsePassword(incomingJSON)):
-            add_text(0, 3, "vrai")
             order = parseData(incomingJSON)[0]
             if (order != DISP_ORD) and (order == "TL" or order == "LT"):
                 DISP_ORD = order
     displayToScreen(DISP_ORD)
-    
     radio.send(getJSONToSend())
-    
-    
-    
-    
-    
-    
-    
-    
-    
